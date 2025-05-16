e2b89eb255f9b09dbbb231b3f887d6c68642be1d
nassarahmad <ahmad.m.nassarr@gmail.com>
2025-05-16 21:09:40 +0300

    create file seed user and changing updates on backend and clean code

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,22 +1,35 @@
 
  import express from "express";
+ import dotenv from "dotenv";
+ import cookieParser from "cookie-parser";
+ import cors from "cors";
+ import path from "path";
+ import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js"
-import dotenv from "dotenv";
-import cookieParser from "cookie-parser";
-import { connectDB } from "./lib/db.js";
+import { app, server } from "./lib/socket.js";
 dotenv.config();
-const app=express();
-
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
+
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+    origin: "http://localhost:5173",
+    Credentials: true,}
+))
 app.use("/api/auth",authRoutes)
 app.use("/api/message",messageRoutes
 
 )
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+    }
 
-app.listen(5001,()=>{
+server.listen(5001,()=>{
     console.log("server is running on port PORT:"+ PORT);
     connectDB();
     
