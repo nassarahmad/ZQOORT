d0a5ec00743687b5c19550485772784445466857
nassarahmad <ahmad.m.nassarr@gmail.com>
2025-05-16 14:34:42 +0300

    ive created nessarry request for user and auth

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,21 @@
 
  import express from "express";
 import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js"
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import { connectDB } from "./lib/db.js";
 dotenv.config();
 const app=express();
+
 const PORT = process.env.PORT;
 app.use(express.json());
+app.use(cookieParser());
 app.use("/api/auth",authRoutes)
+app.use("/api/message",messageRoutes
+
+)
+
 app.listen(5001,()=>{
     console.log("server is running on port PORT:"+ PORT);
     connectDB();
