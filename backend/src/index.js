25a69bdfe36546477d5d1f249cec72ed2094b956
nassarahmad <ahmad.m.nassarr@gmail.com>
2025-05-16 12:35:21 +0300

    authController+userModel

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,19 @@
 
-import express from "express";
+ import express from "express";
 import authRoutes from "./routes/auth.routes.js";
-
+import dotenv from "dotenv";
+import { connectDB } from "./lib/db.js";
+dotenv.config();
 const app=express();
-
+const PORT = process.env.PORT;
+app.use(express.json());
 app.use("/api/auth",authRoutes)
 app.listen(5001,()=>{
-    console.log("server started on port 5001");
+    console.log("server is running on port PORT:"+ PORT);
+    connectDB();
     
 })
+ 
+
+
+
